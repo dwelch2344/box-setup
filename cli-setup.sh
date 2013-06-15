c54f21f803bf3d86ef46d92ea28c7256684cdeb8
David Welch <david@davidwelch.co>
2013-06-15 17:27:14 -0500

    Added initial slate settings

--- cli-setup.sh
+++ cli-setup.sh
@@ -7,3 +7,11 @@ brew doctor
 # configure git
 brew install git
 git config --global --add color.ui true
+
+
+
+
+# Install Slate
+cd /Applications && curl http://www.ninjamonkeysoftware.com/slate/versions/slate-latest.tar.gz | tar -xz
+# TODO - download slate from home GitHub to ~/.slate
+# TODO - launch slate
